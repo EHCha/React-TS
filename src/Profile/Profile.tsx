96e491386511f77b0d1f04ace0d2ce6db168eec3
EHCha <chaeh0926@gmail.com>
2023-01-27 17:57:03 +0900

    first TS

---
+++ src/Profile/Profile.tsx
@@ -0,0 +1,25 @@
+import '../Profile/Profile.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+function Profile() {
+    const [userInfo, setUserInfo] = useState<any>(null);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
+            setUserInfo(res.data);
+        });
+    }, []);
+    console.log(userInfo)
+    return(
+        <div>
+            {userInfo.map((user) => (
+                <li key={user.id}>
+                    {user.username} ({user.name})
+                </li>  
+            ))}
+        </div>
+    );
+}
+
+export default Profile;
